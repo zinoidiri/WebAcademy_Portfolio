fb4a127fd5d996faa1dcf80d4bba3d202778e8a3
zinoidiri <zino.idiri@epitech.eu>
2021-06-12 11:06:40 +0200

    newcommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <>
-      <NAvMenu/>
-    </>
+    <Router>
+      <NavMenu />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavMenuStyles = styled.div;
+// background-color: red;
 
 export default function NavMenu() {
-    return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    )
+  return (
+    <NavMenuStyles>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </NavMenuStyles>
+  );
 }
